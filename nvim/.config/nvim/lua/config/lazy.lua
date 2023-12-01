3cbf24ad150b22d270ff2db91d162547503c3010
zzz <jankin.z@outlook.com>
2023-12-01 12:54:56 +0800

    upgrade lazynvim

--- nvim/.config/nvim/lua/config/lazy.lua
+++ nvim/.config/nvim/lua/config/lazy.lua
@@ -10,11 +10,6 @@ require("lazy").setup({
   spec = {
     -- add LazyVim and import its plugins
     { "LazyVim/LazyVim", import = "lazyvim.plugins" },
-    -- import any extras modules here
-    { import = "lazyvim.plugins.extras.lang.go" },
-    { import = "lazyvim.plugins.extras.lang.rust" },
-    { import = "lazyvim.plugins.extras.dap" },
-    { import = "lazyvim.plugins.extras.coding.yanky" },
     -- import/override with your plugins
     { import = "plugins" },
   },
--- nvim/.config/nvim/lua/plugins/lsp.lua
+++ nvim/.config/nvim/lua/plugins/lsp.lua
@@ -5,21 +5,7 @@ return {
       inlay_hints = {
         enabled = true,
       },
-      autoformat = true,
       format_notify = false,
     },
-  },
-  {
-    "jose-elias-alvarez/null-ls.nvim",
-    enabled = true,
-    opts = function()
-      local nls = require("null-ls")
-      return {
-        sources = {
-          nls.builtins.formatting.stylua,
-          nls.builtins.formatting.shfmt.with({ extra_args = { "-i", "4", "-ci", "-s" } }),
-        },
-      }
-    end,
   },
 }
